d76c589c57759b101b6f7746acaf0918b593af61
W1zheng <527646135@qq.com>
2022-07-14 16:19:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -49,16 +49,16 @@ def test(model, model_fn, data_name, epoch):
 
         matches = {}
         times = 0
-   
+
         for i, data in enumerate(dataloader):
             torch.cuda.empty_cache()
             start1 = time.time()
             preds = model_fn(data, model, epoch)
-                
+
             if preds == None:
                 break
-            
-            coords = data['locs'].cuda()     
+
+            coords = data['locs'].cuda()
             superpoint = data['superpoint']
             ##### get semantic predictions
             semantic_scores = preds['semantic']
@@ -72,9 +72,9 @@ def test(model, model_fn, data_name, epoch):
 
             if (epoch > cfg.prepare_epochs):
                 masks, thres, thre_masks, seg_result, ins_sem_preds = preds['decoding']
-                
+
                 ### post-processing
-                # cover score 
+                # cover score
                 org_num = thre_masks.sum(1) + 1e-6
                 masks_score = torch.zeros(masks.shape[0]).cuda().long() # (I, N), int, cuda
                 masks_num = torch.zeros(masks.shape[0]).cuda().float()
@@ -86,9 +86,9 @@ def test(model, model_fn, data_name, epoch):
                 seg_score, seg_result = thre_masks.max(0)
                 seg_result[seg_score < thres[thres != 0].min()] = -100
 
-                # mask number 
+                # mask number
                 for i in range(masks_score.shape[0]): masks_num[i] = (seg_result == i).sum()
-                num_mask = (masks_num > cfg.TEST_NPOINT_THRESH) 
+                num_mask = (masks_num > cfg.TEST_NPOINT_THRESH)
 
                 merged_pt = merged_pt[num_mask]
                 ins_sem_preds = ins_sem_preds[num_mask]
@@ -102,7 +102,7 @@ def test(model, model_fn, data_name, epoch):
                 # prediction scores
                 semantic_scores_soft = semantic_scores.softmax(-1)
                 scores = torch.zeros_like(cover_scores)
-                for i in range(num_mask.sum()): 
+                for i in range(num_mask.sum()):
                     pts_num = (seg_result == i).sum()
                     scores[i] = thre_masks[i, seg_result == i].sum() / (pts_num + 1e-6)
                     if ins_sem_preds[i] == 20: scores[i] = 0
@@ -123,17 +123,17 @@ def test(model, model_fn, data_name, epoch):
 
                 sp_labels, sp_scores = utils.align_superpoint_label(seg_result, superpoint, thre_masks.shape[0])
                 seg_result_refine = sp_labels[superpoint]
-                
+
                 seg_result = seg_result_refine
-                
+
                 object_idxs = torch.nonzero(semantic_preds >= cfg.invalid_classes).view(-1)
                 ins_sem_preds = utils.get_instance_seg_pred_label(semantic_preds[object_idxs], seg_result[object_idxs], thre_masks.shape[0])
-                
+
                 batch_idxs = coords[:, 0]
-                ins_batch_idxs = batch_idxs[merged_pt]   
-                
+                ins_batch_idxs = batch_idxs[merged_pt]
+
                 end1 = time.time() - start1
-                   
+
                 for idx in range(batch_idxs.max()+1):
                     test_scene_name = dataset.test_file_names[int(data['id'][idx])].split('/')[-1][:12]
                     ins_batch_mask = (ins_batch_idxs == idx)
@@ -153,7 +153,7 @@ def test(model, model_fn, data_name, epoch):
                     masks_category_batch = masks_category_batch[cat_mask]
 
                     semantic_id = torch.tensor(semantic_label_idx, device=masks_category_batch.device)[masks_category_batch.long()]
-                    
+
                     clusters = masks_pred
                     cluster_scores = scores_pred_batch
                     cluster_semantic_id = semantic_id
@@ -220,11 +220,11 @@ def test(model, model_fn, data_name, epoch):
                     ##### print
                     logger.info("instance iter: {}/{} point_num: {} ncluster: {} time: total {:.2f}s inference {:.2f}s save {:.2f}s".format(data['id'][idx] + 1, len(dataset.test_files), N, nclusters, end, end1, end3))
                     times += end1
-                    
+
         ##### evaluation
         if cfg.eval:
             ap_scores = eval.evaluate_matches(matches)
-            np.save(os.path.join(result_dir, 'ap_scores.npy'), ap_scores)
+            #np.save(os.path.join(result_dir, 'ap_scores.npy'), ap_scores)
             avgs = eval.compute_averages(ap_scores)
             eval.print_results(avgs)
 
@@ -234,7 +234,7 @@ def test(model, model_fn, data_name, epoch):
             logger.info("semantic_segmantation_accuracy: {:.4f}".format(seg_accuracy))
             miou = evaluate_semantic_segmantation_miou(matches)
             logger.info("semantic_segmantation_mIoU: {:.4f}".format(miou))
-        
+
         scan_nums = (len(dataset.test_files) * cfg.test_batch_size)
         average_time = times / scan_nums
 
@@ -274,7 +274,7 @@ def evaluate_semantic_segmantation_miou(matches):
     iou_tensor = torch.tensor(iou_list)
     miou = iou_tensor.mean()
     return miou
-        
+
 
 if __name__ == '__main__':
     init()
@@ -288,7 +288,7 @@ if __name__ == '__main__':
     logger.info('=> creating model ...')
     logger.info('Classes: {}'.format(cfg.classes))
 
-    from model.DKNet import DKNet                
+    from model.DKNet import DKNet
     from model.DKNet import test_fn
     model = DKNet(cfg)
     ##### model_fn
